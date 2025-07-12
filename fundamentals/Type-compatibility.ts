2c0a47dd9b106833c3e25b38e245625e2570b67d
vsankaralingam <sankaralingamvenkatsan@gmail.com>
2025-07-12 17:44:09 +0530

    update

---
+++ fundamentals/Type-compatibility.ts
@@ -0,0 +1,11 @@
+interface Person{
+    name1: string;
+    age: number;
+    gender: string;
+}
+function greet(p: Person){
+    console.log('Hello, ${p.name1}');
+}
+
+const user = {name1:"sankara",age: 25, gender:"male"}
+greet(user);
